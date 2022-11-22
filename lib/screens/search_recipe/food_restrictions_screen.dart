288d6a89e59a54c8e8c7118c0ff27b038c1cb1fd
Leticia Deodato <leticiamanuelletd@gmail.com>
2022-11-21 22:02:45 -0300

    Repassando as restrições adiante (ideal seria utilizar flutter modular para injeção de dependencia)

---
+++ lib/screens/search_recipe/food_restrictions_screen.dart
@@ -0,0 +1,275 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:pockectcheff/models/SearchTopics.dart';
+import 'package:pockectcheff/screens/home/home_screen.dart';
+import 'package:pockectcheff/screens/login/register_screen.dart';
+import 'package:pockectcheff/screens/login/reset_password_screen.dart';
+import 'package:pockectcheff/screens/search_recipe/ingredients_screen.dart';
+import 'package:pockectcheff/screens/search_recipe/special_dishes_screen.dart';
+
+class FoodRestrictionsScreen extends StatefulWidget {
+  FoodRestrictionsScreen({super.key, required this.topics});
+  SearchTopics topics;
+
+  @override
+  _FoodRestrictionsScreenState createState() => _FoodRestrictionsScreenState(topics);
+}
+
+class _FoodRestrictionsScreenState extends State<FoodRestrictionsScreen> {
+  _FoodRestrictionsScreenState(this.restrictionsTopics);
+
+  SearchTopics restrictionsTopics;
+  
+  TextEditingController ingredientsController = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey();
+
+  static List<String> restrictions = [];
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(255, 227, 226, 1),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(
+                  15,
+                  30,
+                  15,
+                  30,
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Container(
+                          color: Color.fromARGB(255, 255, 67, 54),
+                          height: 5,
+                          width: 55,
+                        ),
+                        Container(
+                          color: Color.fromARGB(255, 255, 67, 54),
+                          height: 5,
+                          width: 55,
+                        ),
+                        Container(
+                          color: Colors.white,
+                          height: 5,
+                          width: 55,
+                        ),
+                        Container(
+                          color: Colors.white,
+                          height: 5,
+                          width: 55,
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        "Adicione suas restrições",
+                        style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                          color: Color.fromARGB(255, 78, 28, 24),
+                        ),
+                      ),
+                    ),
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        "alimentares",
+                        style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                          color: Color.fromARGB(255, 78, 28, 24),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Form(
+                      key: _formKey,
+                      child: Column(
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Container(
+                                width: 155,
+                                child: TextFormField(
+                                  cursorColor: Color.fromARGB(255, 185, 48, 39),
+                                  keyboardType: TextInputType.emailAddress,
+                                  decoration: const InputDecoration(
+                                    filled: true,
+                                    fillColor: Colors.white,
+                                    border: InputBorder.none,
+                                    hintText: "Restrições",
+                                    hintStyle: TextStyle(
+                                      color: Color.fromARGB(255, 129, 129, 129),
+                                      fontSize: 12,
+                                    ),
+                                  ),
+                                  controller: ingredientsController,
+                                  style: TextStyle(
+                                    color: Color.fromARGB(255, 0, 0, 0),
+                                  ),
+                                  textAlign: TextAlign.left,
+                                  validator: (value) {
+                                    if (value!.isEmpty) {
+                                      return "Insert your ingredient";
+                                    } else {
+                                      return null;
+                                    }
+                                  },
+                                ),
+                              ),
+                              SizedBox(
+                                width: 10,
+                              ),
+                              ButtonTheme(
+                                child: ElevatedButton(
+                                  style: ElevatedButton.styleFrom(
+                                    primary: Color.fromRGBO(255, 83, 71, 1),
+                                  ),
+                                  onPressed: () {
+                                    setState(() {
+                                      restrictions
+                                          .add(ingredientsController.text);
+                                      ingredientsController.clear();
+                                    });
+                                  },
+                                  child: Icon(
+                                    Icons.add,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    restrictions.isNotEmpty
+                        ? Column(
+                            children: restrictions.map((ingredient) {
+                            return Container(
+                              width: 220,
+                              child: Card(
+                                child: ListTile(
+                                  title: Text(ingredient),
+                                  trailing: ElevatedButton(
+                                    style: ElevatedButton.styleFrom(
+                                        primary: Colors.redAccent),
+                                    child: Icon(Icons.delete),
+                                    onPressed: () {
+                                      setState(() {
+                                        restrictions.removeWhere((element) {
+                                          return element == ingredient;
+                                        });
+                                      });
+                                    },
+                                  ),
+                                ),
+                              ),
+                            );
+                          }).toList())
+                        : Container(),
+                    SizedBox(
+                      height: 30,
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                child: Column(
+                  children: [
+                    ButtonTheme(
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          primary: restrictions.isEmpty
+                              ? Colors.grey
+                              : Color.fromRGBO(255, 83, 71, 1),
+                        ),
+                        onPressed: restrictions.isEmpty
+                            ? () {}
+                            : () {
+                                setState(() {
+                                  restrictions.clear();
+                                });
+                              },
+                        child: Text("Excluir tudo"),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        ButtonTheme(
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              primary: Color.fromRGBO(255, 83, 71, 1),
+                            ),
+                            onPressed: () => Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => IngredientsScreen(),
+                              ),
+                            ),
+                            child: Text("Voltar"),
+                          ),
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        ButtonTheme(
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              primary: Color.fromRGBO(255, 83, 71, 1),
+                            ),
+                            onPressed: () {
+                              setState(() {
+                                restrictionsTopics.restrictions = restrictions; 
+                              });
+                              print(">>>>>>>>>>>>>>$restrictions");
+                              print(">>>>>>>>>>>>>>:::::${restrictionsTopics.restrictions}");
+                              Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => SpecialDishesScreen(topics: restrictionsTopics,),
+                              ),
+                            );},
+                            child: Text("Avançar"),
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 50,
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
