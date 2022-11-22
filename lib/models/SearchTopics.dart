b8413463dd2c01278a24980117696a029caf81b9
Leticia Deodato <leticiamanuelletd@gmail.com>
2022-11-21 22:03:49 -0300

    Criando um model para a busca por receita

---
+++ lib/models/SearchTopics.dart
@@ -0,0 +1,13 @@
+class SearchTopics {
+  List<String>? ingredients;
+  List<String>? restrictions;
+  List<String>? specialDishes;
+
+  //TODO: Corrigir Data Class para adicionar o subActionsTitle
+
+  SearchTopics({
+    this.ingredients,
+    this.restrictions,
+    this.specialDishes
+  });
+}
